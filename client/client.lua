085660ec6aedc74903e15162509d6aa0ce57482d
IlMelons <melonss2002@gmail.com>
2025-06-23 19:14:44 +0200

    Better cache handling, -50 KiB+ Memory, Preload menus, debloated useless menu parts

--- client/client.lua
+++ client/client.lua
@@ -1,7 +1,8 @@
 local elevators = lib.load("config.config")
 
-local function ShowMenu(name, floors, label, index)
+local function buildMenuOptions(name, floors)
     local menu = {}
+
     for i = 1, #floors do
         local floorOption = {
             title = floors[i].title,
@@ -13,43 +14,29 @@ local function ShowMenu(name, floors, label, index)
                 if floors[i].locked and not hasAccess then
                     return client.Notify(locale("notify.access-denied"), "error")
                 end
-
                 DoScreenFadeOut(1000)
                 Wait(2000)
-
-                local playerPed = cache.ped or PlayerPedId()
+                local index = LocalPlayer.state.zoneIndex
                 local targetZone = floors[i].zones[index]
                 if not targetZone or not targetZone.coords then
                     targetZone = floors[i].zones[1]
                 end
-                SetEntityCoords(playerPed, targetZone.coords.x, targetZone.coords.y, targetZone.coords.z, false, false, false, false)
-                SetEntityHeading(playerPed, targetZone.coords.w)
-
+                SetEntityCoords(cache.ped, targetZone.coords.x, targetZone.coords.y, targetZone.coords.z, false, false, false, false)
+                SetEntityHeading(cache.ped, targetZone.coords.w)
                 Wait(2000)
                 DoScreenFadeIn(1000)
-
                 client.Notify(locale("notify.reached-floor"), "success")
             end,
-            metadata = {
-                {
-                    label = locale("menu.access"),
-                    value = (floors[i].locked and locale("menu.locked") or locale("menu.unlocked"))
-                }
-            },
         }
         menu[#menu + 1] = floorOption
     end
 
-    local menuId = "elevator_menu"
-    lib.registerContext({
-        id = menuId,
-        title = label,
-        options = menu,
-        onExit = function()
-        end
-    })
+    return menu
+end
 
-    lib.showContext(menuId)
+local function setState(index)
+    local playerState = LocalPlayer.state
+    playerState:set("zoneIndex", index, true)
 end
 
 local function CreateElevator(name, data)
@@ -60,17 +47,19 @@ local function CreateElevator(name, data)
                 size = zoneData.size,
                 rotation = zoneData.coords.w,
                 onEnter = function()
+                    setState(index)
                     lib.showTextUI(locale("textui.elevator"), {
                         position = "left-center",
                         icon = "fa-solid fa-building",
                     })
                 end,
                 onExit = function()
+                    setState(nil)
                     lib.hideTextUI()
                 end,
                 inside = function()
                     if IsControlJustReleased(0, 38) then
-                        ShowMenu(name, data.floors, data.label, index)
+                        lib.showContext(("mnr_elvators:menu:%s"):format(name))
                     end
                 end,
                 debug = elevators[name].debug,
@@ -79,6 +68,17 @@ local function CreateElevator(name, data)
     end
 end
 
+local function CreateElevatorMenu(name, floors, label)
+    local menu = buildMenuOptions(name, floors)
+
+    lib.registerContext({
+        id = ("mnr_elvators:menu:%s"):format(name),
+        title = label,
+        options = menu,
+    })
+end
+
 for name, data in pairs(elevators) do
     CreateElevator(name, data)
+    CreateElevatorMenu(name, data.floors, data.label)
 end
